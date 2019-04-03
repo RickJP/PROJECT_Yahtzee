c225667a3314d464bc0e14d105067417728fb027
Rick <3741326+RickJP@users.noreply.github.com>
2019-04-03 14:07:14 +0900

    Now prohibits five dice to be held; Allows two rolls for each player and disables either 'roll dice' button or 'score card' when not in use

--- app.js
+++ app.js
@@ -1,126 +1,137 @@
-var scores,
-	roundScores,
-	activePlayer,
+var activePlayer,
 	gamePlaying = true,
 	previousRoll = 0,
-	winningScore,
 	newGame = false;
 rollNumber = 0;
 const DICE_DIR = 'images/dice/';
-const DICE_HOLD_DIR = 'images/dice_hold/';
 const DICE_HOLD_PREFIX = 'h';
 init();
 
-// START A NEW GAME
+// START A NEW GAME => Call initialize function
 document.querySelector('.btn-new').addEventListener('click', init);
+
+function getDiceImgFile(diceId) {
+	// Get file name by stripping out the path
+	return document.getElementById(diceId).src.replace(/^.*[\\\/]/, '');
+}
+
+function isHeld(dImg) {
+	// Return whether dice is held or not
+	return dImg.charAt(0) === 'h' ? true : false;
+}
+
+function rollTheDice() {
+	//console.log(`Roll Number ${rollNumber}`);
+	const dicePrefix = 'dice-';
+
+	for (let i = 1; i <= 5; i++) {
+		// if dice not held roll dice, else continue
+		diceImg = getDiceImgFile(`dice-${i}`);
+
+		if (!isHeld(diceImg)) {
+			// if not held then roll
+			diceNum = Math.floor(Math.random() * 6) + 1;
+
+			diceRolled = `${DICE_DIR}${diceNum}.png`;
+			diceRef = `dice-${String(i)}`;
+			document.getElementById(diceRef).style.display = 'block';
+			document.getElementById(diceRef).src = diceRolled;
+		} else {
+			continue;
+		}
+	}
+}
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
 	if (gamePlaying) {
+		rollTheDice();
 		rollNumber++;
-		const dicePrefix = 'dice-';
 
-		function rollDice() {
-			number = Math.floor(Math.random() * 6) + 1;
-			return number;
-		}
+		// Disable the roll button
+		document.querySelector('.btn-roll').disabled = true;
 
-		//console.log(`Roll Number ${rollNumber}`);
-		for (let i = 1; i <= 5; i++) {
-			number = rollDice();
-			diceRolled = DICE_DIR + number + '.png';
-			diceRef = dicePrefix + String(i);
-
-			//console.log(`${diceRef} : ${diceRolled}`);
-
-			document.getElementById(diceRef).style.display = 'block';
-			document.getElementById(diceRef).src = diceRolled;
-		}
-		if (rollNumber === 2) {
-			rollNumber = 0;
-			//playerDecides();
-		}
+		// Enable category selection for current player (activePlayer)
+		console.log('Active player: ' + activePlayer);
+		document.getElementById(`s-card-${activePlayer}`).style.pointerEvents = 'auto';
 	}
 });
 
-function getDiceFile(diceRef) {
-	var fullPath = document.getElementById(diceRef).src;
-	var fileName = fullPath.replace(/^.*[\\\/]/, '');
-	//var relPath = fullPath.replace(/^(?:\/\/|[^\/]+)*\//, '');
-	return fileName;
-}
+// Get selection from player 1
+document.getElementById('s-card-0').addEventListener('click', (e) => {
+	// Disable category selection
+	document.getElementById('s-card-0').style.pointerEvents = 'none';
 
-function toggleHold(f, dRef) {
-	// Check for 'h' as first letter (i.e. check for hold dice)
-	if (f[0] === 'h') {
-		// remove 'h' from file name
-		f = f.substr(1);
-		// set dice to active
-		document.getElementById(dRef).src = DICE_DIR + f;
-	} else {
-		// set dice to hold
-		document.getElementById(dRef).src = DICE_HOLD_DIR + DICE_HOLD_PREFIX + f;
+	// Enable roll button
+	document.querySelector('.btn-roll').disabled = false;
+
+	if (rollNumber === 2) {
+		removeAllDice();
+		nextPlayer();
 	}
-}
 
-document.getElementById('dice-1').addEventListener('click', () => {
-	diceRef = 'dice-1';
-	file = getDiceFile(diceRef);
-	toggleHold(file, diceRef);
-	console.log(file);
+	console.log(e.target.classList.value);
 });
-document.getElementById('dice-2').addEventListener('click', () => {
-	diceRef = 'dice-2';
-	file = getDiceFile(diceRef);
-	toggleHold(file, diceRef);
+
+// Get selection from player 2
+document.getElementById('s-card-1').addEventListener('click', (e) => {
+	// Disable category selection
+	document.getElementById('s-card-1').style.pointerEvents = 'none';
+	// Enable roll button
+	document.querySelector('.btn-roll').disabled = false;
+
+	console.log(e.target.classList.value);
 });
-document.getElementById('dice-3').addEventListener('click', () => {
-	diceRef = 'dice-3';
-	file = getDiceFile(diceRef);
-	toggleHold(file, diceRef);
-});
-document.getElementById('dice-4').addEventListener('click', () => {
-	diceRef = 'dice-4';
-	file = getDiceFile(diceRef);
-	toggleHold(file, diceRef);
-});
-document.getElementById('dice-5').addEventListener('click', () => {
-	diceRef = 'dice-5';
-	file = getDiceFile(diceRef);
-	toggleHold(file, diceRef);
+
+// document.querySelector('.score-card').addEventListener('click', (e) => {
+// 	console.log('clicked score card');
+// });
+
+// Allow dice to be held or not
+document.getElementById('dice-group').addEventListener('click', (e) => {
+	let numHeld = 0;
+	for (let i = 1; i < 6; i++) {
+		dImg = document.getElementById(`dice-${i}`).src.replace(/^.*[\\\/]/, '');
+		if (isHeld(dImg)) {
+			numHeld++;
+		}
+	}
+	diceImg = e.target.src.replace(/^.*[\\\/]/, '');
+
+	if (numHeld !== 4 || isHeld(diceImg)) {
+		// Do not allow five dice to be held, but allow other dice to be unheld
+		toggledDiceImg = diceImg.charAt(0) === 'h' ? diceImg.substr(1) : `h${diceImg}`;
+		document.getElementById(e.target.id).src = `${DICE_DIR}${toggledDiceImg}`;
+	}
 });
 
 function nextPlayer() {
 	activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-	roundScore = 0;
 	document.querySelector('.player-0-panel').classList.toggle('active');
 	document.querySelector('.player-1-panel').classList.toggle('active');
-
 	removeAllDice();
 }
 
 function removeAllDice() {
+	// Clear all dice from the UI
 	for (let i = 1; i <= 5; i++) {
-		diceRef = 'dice-' + i;
-		document.getElementById(diceRef).style.display = 'none';
+		document.getElementById(`dice-${i}`).style.display = 'none';
 	}
 }
 
 function init() {
-	scores = [ 0, 0 ];
 	activePlayer = 0;
-	roundScore = 0;
 	gamePlaying = true;
 	rollNumber = 0;
-
 	removeAllDice();
 
 	document.getElementById('name-0').textContent = 'Player 1';
 	document.getElementById('name-1').textContent = 'Player 2';
-
 	document.querySelector('.player-0-panel').classList.remove('winner');
 	document.querySelector('.player-1-panel').classList.remove('winner');
 	document.querySelector('.player-0-panel').classList.remove('active');
 	document.querySelector('.player-1-panel').classList.remove('active');
-
 	document.querySelector('.player-0-panel').classList.add('active');
+
+	document.getElementById('s-card-0').style.pointerEvents = 'none';
+	document.getElementById('s-card-1').style.pointerEvents = 'none';
 }
