42c4e564377faf5245ce667caaa194f9bac72505
Rick <3741326+RickJP@users.noreply.github.com>
2019-04-07 21:54:01 +0900

    From my light testing, it seems to play well (no bugs found),  with correct scoring for upper, lower and grand total; End game condition coded with simple log to console

--- app.js
+++ app.js
@@ -2,16 +2,34 @@ let activePlayer = 0,
 	gamePlaying = true,
 	previousRoll = 0,
 	newGame = false,
-	rollNumber,
+	rollCount,
 	diceHeld;
 const DICE_DIR = 'images/dice/';
 const DICE_HOLD_PREFIX = 'h';
 
-// Start init on refresh
+// Initailize game on refresh
 init();
 
 // START A NEW GAME => Call initialize function
 document.querySelector('.btn-new').addEventListener('click', init);
+
+/*** 
+ * 										*
+ *   DICE HANDLING    *
+ * 										*
+ */
+
+function resetDice() {
+	for (let i = 1; i < 6; i++) {
+		diceImg = getDiceImgFile(`dice-${i}`);
+		isHeld(diceImg) ? (diceImg = diceImg.substr(1)) : null;
+		document.getElementById(`dice-${i}`).src = `${DICE_DIR}${diceImg}`;
+		document.getElementById(`dice-${i}`).removeAttribute('value');
+		document.getElementById(`dice-${i}`).classList.remove('held');
+		document.getElementById(`dice-${i}`).style.display = 'none';
+	}
+	document.getElementById('dice-group').style.pointerEvents = 'auto';
+}
 
 function getDiceImgFile(diceId) {
 	// Get file name by stripping out the path
@@ -49,6 +67,12 @@ function rollTheDice() {
 	}
 }
 
+/*** 
+ * 										*
+ *   DICE CALC   			*
+ * 										*
+ */
+
 function getRollTotals() {
 	let rollTotals = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0 };
 	for (let i = 0; i < 5; i++) {
@@ -72,79 +96,122 @@ function calculateScoreForOthers(e) {
 	// Strip out the prefix to leave the number
 	selection = parseInt(e.target.id.substr(2));
 	let score = 0;
-
+	let success = false;
 	currentScore = document.getElementById(scoreId).textContent;
-
 	if (selection > 6 && selection < 14) {
-		//Update the score to the UI
-
-		// TODO
 		let isValid,
 			newScore = 0;
-		isValid = document.getElementById(`s${activePlayer}-${selection}`).getAttribute('valid');
 
-		console.log('IS VALID: ' + isValid);
-		console.log('CURRENT SCORE: ' + currentScore === '0');
-		if (isValid && (currentScore === '0' || currentScore !== '-')) {
-			// SCORE -> CHANCE
-			console.log('ENTERED IF');
-			if (selection === 7) {
-				newScore = calcSumTotalAllDice();
+		// TODO
+		isValid = document.getElementById(`${activePlayer}-${selection}`).classList.contains('valid');
+
+		if (currentScore === '0') {
+			if (isValid) {
+				// SCORE -> CHANCE
+				if (selection === 7) {
+					newScore = calcSumTotalAllDice();
+				}
+				// SCORE -> 4 OF A KIND
+				if (selection === 8) {
+					newScore = calcSumTotalAllDice();
+				}
+				// SCORE -> 3 OF A KIND
+				if (selection === 9) {
+					newScore = calcSumTotalAllDice();
+				}
+				// SCORE -> SMALL STRAIGHT
+				if (selection === 10) {
+					newScore = 30;
+				}
+				// SCORE -> LARGE STRAIGHT
+				if (selection === 11) {
+					newScore = 40;
+				}
+				// SCORE -> FULL HOUSE
+				if (selection === 12) {
+					newScore = 25;
+				}
+				// SCORE -> YAHTZEE
+				if (selection === 13) {
+					newScore = 50;
+				}
+			} else {
+				newScore = '-';
 			}
-			// SCORE -> 4 OF A KIND
-			if (selection === 8) {
-				newScore = calcSumTotalAllDice();
-			}
-			// SCORE -> 3 OF A KIND
-			if (selection === 9) {
-			}
-			// SCORE -> SMALL STRAIGHT
-			if (selection === 10) {
-			}
-			// SCORE -> LARGE STRAIGHT
-			if (selection === 11) {
-			}
-			// SCORE -> FULL HOUSE
-			if (selection === 12) {
-			}
-			// SCORE -> YAHTZEE
-			if (selection === 13) {
-			}
-			console.log('NEW SCORE: ' + newScore);
 			document.getElementById(scoreId).textContent = newScore;
+			return (success = true);
 		}
 	}
 }
 
+function updateTotals() {
+	scoreElements = document.getElementById(`the-scores-${activePlayer}`).children;
+	let uTotal = (lTotal = 0);
+
+	// Calculate Totals for upper section
+	for (let i = 0; i < 7; i++) {
+		if (scoreElements[i].textContent !== '-') {
+			uTotal += parseInt(scoreElements[i].textContent);
+		}
+	}
+	document.getElementById(`s${activePlayer}-ut`).textContent = uTotal;
+
+	for (let i = 9; i < 15; i++) {
+		if (scoreElements[i].textContent !== '-') {
+			lTotal += parseInt(scoreElements[i].textContent);
+		}
+	}
+	document.getElementById(`s${activePlayer}-lt`).textContent = lTotal;
+
+	document.getElementById(`s${activePlayer}-t`).textContent = uTotal + lTotal;
+	//TODO
+}
+
+/*** 
+ * 											*
+ *   PLAYER OPTIONS     *
+ * 											*
+ */
+
 function setOptionsListTo(action = 'none') {
 	for (let i = 1; i < 14; i++) {
-		// Reset BG Color
-		//document.getElementById(`${activePlayer}-${i}`).style.backgroundColor = '#d9e0f8de';
-		// Set options to diabled
 		document.getElementById(`${activePlayer}-${i}`).style.pointerEvents = action;
 	}
 }
 
 function highlightOption(opt, allowSelection) {
-	if (document.getElementById(`${activePlayer}-${opt}`).className !== 'highlight') {
-		document.getElementById(`${activePlayer}-${opt}`).classList.add('highlight');
+	if (document.getElementById(`${activePlayer}-${opt}`).className !== 'valid') {
+		document.getElementById(`${activePlayer}-${opt}`).classList.add('valid');
 	}
-	//document.getElementById(`${activePlayer}-${opt}`).style.pointerEvents = allowSelection;
 }
 
 function highlightOrNot(num) {
 	checkOption = document.getElementById(`s${activePlayer}-${num}`).textContent;
 
 	// Check whether score is possible for an option and highlight it, if it is.
-	if (checkOption === '0' && checkOption !== '-') {
+	if (checkOption === '0') {
 		// Highlight the options that will give a score
-		if (rollNumber === 1) {
+		if (rollCount % 2 === 1) {
+			// 1st ROLL
 			highlightOption(num, 'none');
 		} else {
 			highlightOption(num, 'auto');
-			document.getElementById(`s${activePlayer}-${num}`).setAttribute('valid', true);
+			//document.getElementById(`s${activePlayer}-${num}`).setAttribute('valid', true);  //?
 		}
 	}
+}
+
+function anyOptionsRemaining(player = activePlayer) {
+	sCardItems = document.getElementById(`the-scores-${player}`).children;
+	noOfItems = sCardItems.length;
+
+	for (let i = 0; i < noOfItems; i++) {
+		if (sCardItems[i].textContent === '0' && i !== 7 && i !== 15 && i !== 16) {
+			// Search for a zero (ie. an option not selected, but do not include the upper, lower & grand totals)
+			return true;
+		}
+	}
+	return false;
 }
 
 function showOptionsAndValidate(allDiceHeld = false) {
@@ -154,10 +221,8 @@ function showOptionsAndValidate(allDiceHeld = false) {
 		rolledA3 = false;
 
 	if (allDiceHeld) {
-		rollNumber = 2;
+		rollCount++;
 	}
-
-	console.log(rollTotals);
 
 	// Highlight options
 	for (let i = 1; i < 7; i++) {
@@ -186,7 +251,6 @@ function showOptionsAndValidate(allDiceHeld = false) {
 			} else if (rollTotals[i] === 5) {
 				highlightOrNot(13); // YAHTZEE!
 			}
-
 			if (countForStraight === 4) {
 				// Got a small straight
 				highlightOrNot(10);
@@ -199,66 +263,96 @@ function showOptionsAndValidate(allDiceHeld = false) {
 	highlightOrNot(7);
 }
 
+/*** 
+ * 																*
+ *   CALCULATE & UPDATE SCORES   	*
+ * 																*
+ */
+
 function calculateScoreForNumbers(e) {
-	//disableOptions();
-	// Get choice from player (target ID)
+	let success = false;
+
 	scoreId = `s${e.target.id}`;
 	// Strip out the prefix to leave the number
 	selection = e.target.id.substr(2);
 
+	let newScore = 0;
+	currentScore = document.getElementById(scoreId).textContent;
+
 	if (selection > 0 && selection < 7) {
-		// Use the number to calculate the score
-		score = rollTotals[selection] * selection;
-		// Update the score to the UI
-		if (score === 0) {
-			score = '-';
+		if (currentScore === '0') {
+			newScore = rollTotals[selection] * selection;
+			// Update the score to the UI
+			newScore === 0 ? (newScore = '-') : null;
+			document.getElementById(scoreId).textContent = newScore;
+			success = true;
 		}
-		document.getElementById(scoreId).textContent = score;
 	}
+	return success;
 }
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
 	if (gamePlaying) {
 		rollTheDice();
-		rollNumber++;
+		rollCount++;
 
 		showOptionsAndValidate();
 
-		if (rollNumber === 2) {
+		if (rollCount % 2 === 0) {
 			// Disable the roll button
 			document.querySelector('.btn-roll').disabled = true;
 			document.querySelector('.btn-roll').classList.toggle('disabled');
 			// Disable dice group on second roll
 			document.getElementById('dice-group').style.pointerEvents = 'none';
 
-			clearAllHighlights();
 			setOptionsListTo('auto');
 			showOptionsAndValidate();
+			diceHeld = 0;
 		}
 	}
 });
 
+/*** 
+ * 																*
+ *   GET CHOICE FROM PLAYER   		*
+ * 																*
+ */
+
 // Listen for click on score card
 [ ...document.querySelectorAll('.score-card') ].forEach((item) => {
 	item.addEventListener('click', (e) => {
-		console.log('ROLL NO. ' + rollNumber);
-		if (rollNumber === 2) {
-			calculateScoreForNumbers(e);
-			calculateScoreForOthers(e);
-			//setOptionsListTo('auto');
-			nextPlayer();
+		if (rollCount % 2 === 0) {
+			// 2nd Roll
+			let success1 = calculateScoreForNumbers(e);
+			let success2 = calculateScoreForOthers(e);
+			if (success1 || success2) {
+				updateTotals(); //TODO
+				nextPlayer();
+			}
 		}
 	});
 });
 
+/*** 
+ * 																*
+ *   TO HOLD. OR NOT TO HOLD?  		*
+ * 																*
+ */
+
 // Allow dice to be held or not
 document.getElementById('dice-group').addEventListener('click', (e) => {
 	diceImg = e.target.src.replace(/^.*[\\\/]/, '');
-	toggledDiceImg = diceImg.charAt(0) === 'h' ? diceImg.substr(1) : `h${diceImg}`;
-
+	if (diceImg.charAt(0) === 'h') {
+		toggledDiceImg = diceImg.substr(1); // Change image to unheld
+		diceHeld--;
+	} else {
+		toggledDiceImg = `h${diceImg}`; // Change image to held
+		diceHeld++;
+	}
 	document.getElementById(e.target.id).src = `${DICE_DIR}${toggledDiceImg}`;
 	document.getElementById(e.target.id).classList.toggle('held');
-	diceHeld++;
+
+	// If 5 dice are held, then disable roll and get selection from player
 	if (diceHeld === 5) {
 		// Disable the roll button
 		document.querySelector('.btn-roll').disabled = true;
@@ -266,17 +360,18 @@ document.getElementById('dice-group').addEventListener('click', (e) => {
 		// Disable dice group on second roll
 		document.getElementById('dice-group').style.pointerEvents = 'none';
 		showOptionsAndValidate(true); // All dice held = true to enable selection
+		setOptionsListTo('auto');
 		diceHeld = 0;
 	}
 });
 
-function clearAllHighlights(player = activePlayer) {
-	// TODO
+function clearHighlightsAndPointerEvents(player = activePlayer) {
 	sCardItems = document.getElementById(`s-card-${player}`).children;
 	noOfItems = sCardItems.length;
 
 	for (let i = 0; i < noOfItems; i++) {
-		sCardItems[i].classList.remove('highlight');
+		sCardItems[i].classList.remove('valid');
+		sCardItems[i].style.pointerEvents = 'none';
 	}
 }
 
@@ -284,51 +379,43 @@ function nextPlayer() {
 	document.querySelector('.btn-roll').disabled = false;
 	document.querySelector('.btn-roll').classList.toggle('disabled');
 	document.getElementById(`s-card-${activePlayer}`).style.pointerEvents = 'none';
+	document.getElementById(`player-${activePlayer}`).classList.toggle('active');
 
 	resetDice();
-	clearAllHighlights();
+	clearHighlightsAndPointerEvents();
 
 	activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-	rollNumber = 0;
-
-	// document.querySelector('.player-0-panel').classList.toggle('active');
-	// document.querySelector('.player-1-panel').classList.toggle('active');
-
+	document.getElementById(`player-${activePlayer}`).classList.toggle('active');
 	setOptionsListTo('none');
-}
-function resetDice() {
-	for (let i = 1; i < 6; i++) {
-		diceImg = getDiceImgFile(`dice-${i}`);
-		isHeld(diceImg) ? (diceImg = diceImg.substr(1)) : null;
-		document.getElementById(`dice-${i}`).src = `${DICE_DIR}${diceImg}`;
-		document.getElementById(`dice-${i}`).removeAttribute('value');
-		document.getElementById(`dice-${i}`).classList.remove('held');
-		document.getElementById(`dice-${i}`).style.display = 'none';
+
+	// TODO
+	if (!anyOptionsRemaining(0)) {
+		console.log('GAME OVER FOR YOU, PLAYER 1.');
 	}
-	document.getElementById('dice-group').style.pointerEvents = 'auto';
+	if (!anyOptionsRemaining(1)) {
+		console.log('GAME OVER FOR YOU, PLAYER 2.');
+	}
 }
 
 function init() {
 	activePlayer = 0;
 	gamePlaying = true;
-	rollNumber = 0;
+	(rollCount = 0), (diceHeld = 0);
 
-	clearAllHighlights(0);
-	clearAllHighlights(1);
+	clearHighlightsAndPointerEvents(0);
+	clearHighlightsAndPointerEvents(1);
 	document.querySelector('.btn-roll').classList.remove('disabled');
 	document.querySelector('.btn-roll').disabled = false;
 
-	// document.getElementById('name-0').textContent = 'Player 1';
-	// document.getElementById('name-1').textContent = 'Player 2';
-	// document.querySelector('.player-0-panel').classList.remove('active');
-	// document.querySelector('.player-1-panel').classList.remove('active');
-
-	// document.querySelector('.player-0-panel').classList.add('active');
+	document.getElementById('player-0').textContent = 'Mike';
+	document.getElementById('player-1').textContent = 'Jeff';
+	document.getElementById('player-0').classList.remove('active');
+	document.getElementById('player-1').classList.remove('active');
+	document.getElementById('player-0').classList.toggle('active');
 
 	document.getElementById('s-card-0').style.pointerEvents = 'none';
 	document.getElementById('s-card-1').style.pointerEvents = 'none';
-
-	diceHeld = 0;
+	updateTotals();
 	resetDice();
 	setOptionsListTo('none');
 }
